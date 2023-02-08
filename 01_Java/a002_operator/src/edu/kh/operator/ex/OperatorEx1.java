7e52275464898ca81d2a2607ed1b16b8fb22882d
baekdh0206 <baekdh@iei.or.kr>
2023-02-08 11:51:32 +0900

    20230208_연산자

---
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -0,0 +1,17 @@
+package edu.kh.operator.ex;
+
+import java.util.Scanner; // Scanner import 코드
+
+public class OperatorEx1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); // Scanner 객체 생성
+		
+		
+	}
+
+}
+
+
+
