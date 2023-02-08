045de7975c2a6886429401e6d01eeb79fa0db5b1
baekdh0206 <baekdh@iei.or.kr>
2023-02-08 12:41:04 +0900

    20230208_산술연산자

--- 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -8,6 +8,29 @@ public class OperatorEx1 {
 		
 		Scanner sc = new Scanner(System.in); // Scanner 객체 생성
 		
+		// 산술 연산자 : +  -  *  /  %(modulo, mod)(나머지)
+		
+		System.out.println("두 정수를 입력 받아 산술 연산 결과 출력하기");
+		
+		System.out.print("정수 입력 1 : ");
+		int input1 = sc.nextInt();
+		
+		System.out.print("정수 입력 2 : ");
+		int input2 = sc.nextInt();
+		
+		// \n : 줄바꿈 (탈출문자)
+		
+		System.out.printf("%d + %d = %d \n", input1, input2, input1+input2);
+		
+		System.out.printf("%d - %d = %d \n", input1, input2, input1-input2);
+		
+		System.out.printf("%d * %d = %d \n", input1, input2, input1*input2);
+		
+		System.out.printf("%d / %d = %d \n", input1, input2, input1/input2);
+		
+		System.out.printf("%d %% %d = %d \n", input1, input2, input1%input2);
+		// printf에서 '%' 글자를 출력하고 싶으면 "%%"를 작성
+		
 		
 	}
 
