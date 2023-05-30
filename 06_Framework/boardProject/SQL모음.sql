1240508f23803a886208d5c495002e46c9adba5a
baekdh0206 <baekdh@iei.or.kr>
2023-05-30 15:12:05 +0900

    무언가 수정됨

--- 06_Framework/boardProject/SQL모음.sql
+++ 06_Framework/boardProject/SQL모음.sql
@@ -1,13 +1,13 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- 계정 생성
-CREATE USER project IDENTIFIED BY "project1234";
+CREATE USER spring IDENTIFIED BY "spring1234!";
 
 -- 권한 부여
-GRANT CONNECT, RESOURCE, CREATE VIEW TO project;
+GRANT CONNECT, RESOURCE, CREATE VIEW TO spring;
 
 -- 객체 생성 공간 할당
-ALTER USER project DEFAULT TABLESPACE SYSTEM
+ALTER USER spring DEFAULT TABLESPACE SYSTEM
 QUOTA UNLIMITED ON SYSTEM;
 
 ------------------------------------------------------------
