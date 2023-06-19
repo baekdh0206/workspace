2e805760d2b16261b9abbd773886eaa02da92c7a
baekdh0206 <baekdh@iei.or.kr>
2023-06-19 10:10:44 +0900

    20230619_댓글

---
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/dao/CommentMapper.java
@@ -0,0 +1,44 @@
+package edu.kh.project.board.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.project.board.model.dto.Comment;
+
+@Mapper
+public interface CommentMapper {
+	
+	/** 댓글 목록 조회
+	 * @param boardNo
+	 * @return cList
+	 */
+	public List<Comment> select(int boardNo);
+
+	
+	/** 댓글 삽입
+	 * @param comment
+	 * @return result
+	 */
+	public int insert(Comment comment);
+
+	
+	/** 댓글 삭제
+	 * @param commentNo
+	 * @return result
+	 */
+	public int delete(int commentNo);
+
+	
+	/** 댓글 수정
+	 * @param comment
+	 * @return result
+	 */
+	public int update(Comment comment);
+	
+	
+
+}
---
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/service/CommentService.java
@@ -0,0 +1,33 @@
+package edu.kh.project.board.model.service;
+
+import java.util.List;
+
+import edu.kh.project.board.model.dto.Comment;
+
+public interface CommentService {
+
+	/** 댓글 목록 조회
+	 * @param boardNo
+	 * @return cList
+	 */
+	List<Comment> select(int boardNo);
+
+	/** 댓글 삽입
+	 * @param comment
+	 * @return result
+	 */
+	int insert(Comment comment);
+
+	/** 댓글 삭제
+	 * @param commentNo
+	 * @return result
+	 */
+	int delete(int commentNo);
+
+	/** 댓글 수정
+	 * @param comment
+	 * @return result
+	 */
+	int update(Comment comment);
+
+}
